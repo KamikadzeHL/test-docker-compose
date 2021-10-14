d8016cc4700b100861eb2d33a3d9c05f03b10ae8
serhii.sallo@gmail.com <serhii.sallo@gmail.com>
2021-10-14 20:40:29 +0300

    Ubuntu 18

---
+++ ubuntu18.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#Docker-ce
+sudo apt-get update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+apt-cache policy docker-ce
+sudo apt install -y docker-ce
+sudo usermod -aG docker ${USER}
+su - ${USER}
+
+
+##Docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
